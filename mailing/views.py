1f3555cad0a8e81f775872673810ec9d6f81830f
artemsav <studentbedn@gmail.com>
2022-08-25 16:53:01 +0300

    add MailingViewSet

--- mailing/views.py
+++ mailing/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
-from rest_framework.serializers import ModelSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from mailing.models import Client, MailingList, Message
 from rest_framework import viewsets
+from rest_framework.serializers import ModelSerializer
 
-class ClientSerializers(ModelSerializer):
+
+class ClientSerializer(ModelSerializer):
 
     class Meta:
         model = Client
@@ -11,7 +14,7 @@ class ClientSerializers(ModelSerializer):
         fields = '__all__'
 
 
-class MailingListSerializers(ModelSerializer):
+class MailingListSerializer(ModelSerializer):
 
     class Meta:
         model = MailingList
@@ -19,7 +22,7 @@ class MailingListSerializers(ModelSerializer):
         fields = '__all__'
 
 
-class MessageSerializers(ModelSerializer):
+class MessageSerializer(ModelSerializer):
 
     class Meta:
         model = Message
@@ -27,4 +30,51 @@ class MessageSerializers(ModelSerializer):
         fields = '__all__'
 
 
+class ClientViewSet(viewsets.ModelViewSet):
+    serializer_class = ClientSerializer
+    queryset = Client.objects.all()
 
+
+class MessageViewSet(viewsets.ModelViewSet):
+    serializer_class = MessageSerializer
+    queryset = Message.objects.all()
+
+
+class MailingViewSet(viewsets.ModelViewSet):
+    serializer_class = MailingListSerializer
+    queryset = MailingList.objects.all()
+
+    @action(detail=True, methods=['get'])
+    def get_info(self, request, pk=None):
+        """
+        Summary data for a specific mailing list
+        """
+        #mailings = MailingList.objects.all()
+        #get_object_or_404(mailings, pk=pk)
+        messages = Message.objects.filter(mailing_id=pk).all()
+        serializer = MessageSerializer(messages, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'])
+    def get_fullinfo(self, request):
+        """
+        Summary data for all mailings
+        """
+        total_count = MailingList.objects.count()
+        mailing = MailingList.objects.values('id')
+        content = {'Total number of mailings': total_count,
+                   'The number of messages sent': ''}
+        result = {}
+
+        for row in mailing:
+            res = {'Total messages': 0, 'Sent': 0, 'No sent': 0}
+            mail = Message.objects.filter(mailing_id=row['id']).all()
+            group_sent = mail.filter(sending_status='Sent').count()
+            group_no_sent = mail.filter(sending_status='No sent').count()
+            res['Total messages'] = len(mail)
+            res['Sent'] = group_sent
+            res['No sent'] = group_no_sent
+            result[row['id']] = res
+
+        content['The number of messages sent'] = result
+        return Response(content)
